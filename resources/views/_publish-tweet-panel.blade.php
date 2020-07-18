1e5025256d898763d7b7468a513fe7b72542173f
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-07-18 19:46:15 +1200

    Update the views for the app

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,23 @@
+
+<div class="border border-blue-400 rounded-lg py-6 px-8 mb-8">
+    <form action="">
+        <textarea
+            name="body"
+            class="w-full"
+            placeholder="What's up guys?"
+        >
+        </textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img src="https://i.pravatar.cc/40" class="rounded-full mr-2" alt="">
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">
+                Tweet-a-roo!
+            </button>
+        </footer>
+    </form>
+</div>
+
+            
+        
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex">
-        <div class="flex-1">
+    <div class="lg:flex">
+        <div class="lg:flex-1">
             @include ('_sidebar-links')
         </div>
-        <div class="flex-1">2</div>
-        <div class="flex-1">3</div>
+        
+        <div class="lg:flex-2 lg:mx-10">
+            @include('_publish-tweet-panel')
+
+            <div class="border border-gray-300 rounded-lg">
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+
+            </div>
+        <div> 
+        <div class="lg:flex-1 bg-blue-100 rounded-lg">
+            @include ('_friends-list')
+        </div>
     </div>
 @endsection
