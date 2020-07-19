f9c5877180099e158b867fca460faa43456211ac
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-07-19 13:16:06 +1200

    Make tweets dynamic
    Tweets exist as a model with their own factory, controllers
    Users can dynamically see their tweets in their timelines

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,13 +1,13 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50" class="rounded-full mr-2" alt="">
+        <img src="https://i.pravatar.cc/50?u={{ $tweet->user->email }}" class="rounded-full mr-2" alt="">
 
     </div>
 
     <div>
-        <h5 class="font-bold mb-4"> Users Name</h5>
+        <h5 class="font-bold mb-4"> {{ $tweet->user->name }} </h5>
         <p class="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at pellentesque neque. Quisque vitae magna convallis nisl ornare faucibus quis et nisi. Suspendisse at tortor condimentum, porta elit ac, fringilla lorem. In pharetra ullamcorper nunc, id lobortis ligula varius ut. Vivamus enim sem, ultrices non neque a, pulvinar convallis lorem. Nulla facilisi. Proin fringilla nulla vitae augue blandit mattis. Aliquam a quam vitae purus imperdiet egestas ac quis erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer in tortor quis tellus fermentum sagittis. Etiam volutpat lacus in aliquet sollicitudin. Nullam nec euismod ipsum, sit amet finibus est. Phasellus pretium, justo quis posuere ullamcorper, nulla lacus interdum dolor, vel congue libero elit in urna. Pellentesque fringilla sed elit a tempus.
+            {{ $tweet->body }}
         </p>
 
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="lg:flex">
-        <div class="lg:flex-1">
+    <div class="lg:flex lg:justify-center">
+        <div class="lg:w-32">
             @include ('_sidebar-links')
         </div>
-        
-        <div class="lg:flex-2 lg:mx-10">
-            @include('_publish-tweet-panel')
+
+        <div class="lg:flex-1 lg:mx-10 lg:mb-10" style="max-width: 700px">
+            @include ('_publish-tweet-panel')
 
             <div class="border border-gray-300 rounded-lg">
-                @include('_tweet')
-                @include('_tweet')
-                @include('_tweet')
-                @include('_tweet')
-
+                @foreach ($tweets as $tweet)
+                    @include('_tweet')
+                @endforeach
             </div>
-        <div> 
-        <div class="lg:flex-1 bg-blue-100 rounded-lg">
+        </div>
+
+        <div class="lg:w-1/6">
             @include ('_friends-list')
         </div>
     </div>
